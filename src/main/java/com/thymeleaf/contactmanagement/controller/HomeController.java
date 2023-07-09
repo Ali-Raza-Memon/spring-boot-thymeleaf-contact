7e94ca96b07615fe7dfd29333bcc490f7e9847a1
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-07-09 23:13:33 +0500

    Now the mapping is updated and this is correct mapping

---
+++ src/main/java/com/thymeleaf/contactmanagement/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.thymeleaf.contactmanagement.controller;
+
+import com.thymeleaf.contactmanagement.dao.UserRepository;
+import com.thymeleaf.contactmanagement.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private UserRepository userRepository;
+
+//    @GetMapping("/test")
+//    @ResponseBody
+//    public String test(){
+//        User user = new User();
+//        user.setName("Durgesh Tiwari");
+//        user.setEmail("durgesh@gmail.com");
+//        userRepository.save(user);
+//        return "Working";
+//    }
+}
