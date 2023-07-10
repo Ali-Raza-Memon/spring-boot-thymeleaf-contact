50ea291a74483e322dd0ab11935debaf123fa886
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-07-10 10:13:05 +0500

    Sign and Home pages are linked to navbar

--- src/main/java/com/thymeleaf/contactmanagement/controller/HomeController.java
+++ src/main/java/com/thymeleaf/contactmanagement/controller/HomeController.java
@@ -13,14 +13,17 @@ public class HomeController {
         return "home";
     }
 
-
-
     @GetMapping("/about")
     public String about(Model model){
         model.addAttribute("title","About - Smart Contact Manager");
         return "about";
     }
 
+    @GetMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("title","Register - Smart Contact Manager");
+        return "signup";
+    }
 
 
 
